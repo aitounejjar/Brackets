075994368d749595c2c31001d49575434a4954c3
aitounejjar <aitounejjar@gmail.com>
2017-11-02 10:47:58 -0400

    Update and rename Brackets.java to BracketsUtils.java

--- src/main/java/BracketsUtils.java
+++ src/main/java/BracketsUtils.java
@@ -1,14 +1,15 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Stack;
 
 /**
- * Created by maitoune on 11/01/17.
+ * Created by Mouhcine on 11/01/17.
  */
-public class Brackets {
+public class BracketsUtils {
+
+    private BracketsUtils() {
+        // only exists to defeat instantiation
+    }
 
     private static final Map<Character, BracketTypes> sMap = new HashMap<Character, BracketTypes>() {{
         put('(', BracketTypes.LEFT_PAREN);
@@ -22,64 +23,6 @@ public class Brackets {
     // matches any character that's neither a left nor a right parenthesis, brace or square bracket
     private static final String regex = ".*[^()\\[\\]{}].*";
 
-    public static void main(String[] args) {
-
-        System.out.println();
-
-        //Scanner sc = new Scanner(System.in);
-        Scanner sc = null;
-        try {
-            sc = new Scanner(new File("src/main/java/input.txt"));
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
-
-        int count = 0;
-        while (sc.hasNextLine()) {
-            /*String input = sc.nextLine();
-            boolean result = isWellFormed(input);
-            System.out.println(count + ":" + result);
-            count++;*/
-
-            Runnable runnable = new RunnableBracketMatcher(++count, sc.nextLine());
-            Thread t = new Thread(runnable);
-            t.start();
-
-        }
-    }
-
-    public enum BracketTypes {
-        LEFT_PAREN ('(', ')', true), RIGHT_PAREN (')', '(', false),
-        LEFT_CURLY ('{', '}', true), RIGHT_CURLY ('}', '{', false),
-        LEFT_SQUARE('[', ']', true), RIGHT_SQUARE(']', '[', false);
-
-        private char mChar;
-        private char mOtherChar;
-        private boolean mIsLeft;
-
-        BracketTypes(char pChar, char pOtherChar, boolean pIsLeft) {
-            mChar = pChar;
-            mOtherChar = pOtherChar;
-            mIsLeft = pIsLeft;
-        }
-
-        public char getChar() {
-            return mChar;
-        }
-
-        public char getClosingChar() {
-            return mOtherChar;
-        }
-
-        public boolean isLeft() {
-            return mIsLeft;
-        }
-
-        public boolean isRight() {
-            return !mIsLeft;
-        }
-    }
-
     public static boolean isWellFormed(String pString) {
 
         // strings that are null/empty and strings with an odd length
@@ -87,26 +30,24 @@ public class Brackets {
             return false;
         }
 
-
         if (pString.matches(regex)) {
             return false;
         }
 
         // now we're guaranteed that our String contains only brackets
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
 
+        boolean leftSquareRead = false;
         for (int i=0; i<pString.length(); ++i) {
             char c = pString.charAt(i);
             BracketTypes bracket = sMap.get(c);
             if (bracket.isLeft()) {
-
                 if (!stack.isEmpty()) {
                     boolean isAllowed = isAllowed(stack.peek(), c);
                     if (!isAllowed) {
                         return false;
                     }
                 }
-
                 // add to stack
                 stack.add(c);
             } else {
@@ -131,6 +72,11 @@ public class Brackets {
         return stack.isEmpty();
     }
 
+    /**
+     * @param pLeft1 a left character
+     * @param pLeft2 a left character
+     * @return true if pLeft2 can appear right after pLeft1
+     */
     private static boolean isAllowed(char pLeft1, char pLeft2) {
         boolean result = true;
 
