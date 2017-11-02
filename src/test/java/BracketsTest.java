08684f2738d6a03c42b09273a70c8621e7b0cd6d
aitounejjar <aitounejjar@gmail.com>
2017-11-02 10:50:18 -0400

    Update BracketsTest.java

--- src/test/java/BracketsTest.java
+++ src/test/java/BracketsTest.java
@@ -4,81 +4,87 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 /**
- * Created by maitoune on 11/01/17.
+ * Created by Mouhcine on 11/01/17.
  */
 
-public class BracketsTest extends TestCase {
+public class BracketsUtilsTest extends TestCase {
 
     @Test
     public void test_empty() {
         boolean expected = false;
-        boolean result = Brackets.isWellFormed("");
+        boolean result = BracketsUtils.isWellFormed("");
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void test_null() {
         boolean expected = false;
-        boolean result = Brackets.isWellFormed(null);
+        boolean result = BracketsUtils.isWellFormed(null);
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void test_nonBracketCharacters() {
-        Assert.assertEquals(false, Brackets.isWellFormed("(a)"));
-        Assert.assertEquals(false, Brackets.isWellFormed("()a"));
-        Assert.assertEquals(false, Brackets.isWellFormed("(123)"));
-        Assert.assertEquals(false, Brackets.isWellFormed("({-})"));
-        Assert.assertEquals(false, Brackets.isWellFormed("[[x[]]]"));
-        Assert.assertEquals(false, Brackets.isWellFormed("[[[[[[[[[[[[[]]]]]]]]]]]]]]]{}{}{}{}{}'{}{}{}{}{}()(((("));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("(a)"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("()a"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("(123)"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("({-})"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("[[x[]]]"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("[[[[[[[[[[[[[]]]]]]]]]]]]]]]{}{}{}{}{}'{}{}{}{}{}()(((("));
     }
 
     @Test
     public void test_openingsAndClosings() {
-        Assert.assertEquals(true, Brackets.isWellFormed("[({})]"));
-        Assert.assertEquals(true, Brackets.isWellFormed("{}{}{}()()(){[[[({[]})]]]}"));
-        Assert.assertEquals(true, Brackets.isWellFormed("{}(){}()[][][]{}()[]"));
-        Assert.assertEquals(true, Brackets.isWellFormed("{[{[({[[[]]]})]}]}"));
-        Assert.assertEquals(false, Brackets.isWellFormed("{}}"));
-        Assert.assertEquals(false, Brackets.isWellFormed("{{}}]"));
-        Assert.assertEquals(false, Brackets.isWellFormed("(()"));
-        Assert.assertEquals(false, Brackets.isWellFormed("({)}"));
-        Assert.assertEquals(true, Brackets.isWellFormed("[(){}]")); // TBD: flagged as false by the provided results.txt
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("[({})]"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("{}{}{}()()(){[[[({[]})]]]}"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("{}(){}()[][][]{}()[]"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("{[{[({[[[]]]})]}]}"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("{}}"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("{{}}]"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("(()"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("({)}"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("[[]]"));
+
+        // TBD: flagged as false by the provided results.txt, not sure why.
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("[(){}]"));
 
     }
 
     @Test()
     public void test_rule_1() {
         // Inside parenthesis () only braces {} are allowed
-        Assert.assertEquals(true, Brackets.isWellFormed("({})"));
-        Assert.assertEquals(false, Brackets.isWellFormed("([])"));
-        Assert.assertEquals(false, Brackets.isWellFormed("(())"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("({})"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("([])"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("([][][][])"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("(())"));
     }
 
     @Test
     public void test_rule_2() {
         // Inside braces {} only square brackets [] are allowed
-        Assert.assertEquals(true, Brackets.isWellFormed("{[]}"));
-        Assert.assertEquals(false, Brackets.isWellFormed("{()}"));
-        Assert.assertEquals(false, Brackets.isWellFormed("{{}}"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("{[]}"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("{()}"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("{{}}"));
     }
 
     @Test
     public void test_rule_3() {
-        // Inside parenthesis () only braces {} are allowed
-        Assert.assertEquals(true, Brackets.isWellFormed("[()]"));
-        Assert.assertEquals(true, Brackets.isWellFormed("[{}]"));
-        Assert.assertEquals(true, Brackets.isWellFormed("[[]]"));
-        Assert.assertEquals(true, Brackets.isWellFormed("[[[]]]"));
-        Assert.assertEquals(false, Brackets.isWellFormed("[([])]"));
+        // Any bracket can appear (directly) inside square brackets []
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("[()]"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("[{}()[]]"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("[[]]"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("[[[]]]"));
+        Assert.assertEquals(false, BracketsUtils.isWellFormed("[([])]"));
     }
 
     @Test
     public void test_rule_4() {
         // You can use a list of braces where a single one is allowed of that type
-        Assert.assertEquals(true, Brackets.isWellFormed("[()()]"));
-        Assert.assertEquals(true, Brackets.isWellFormed("{[][()()]}"));
-        Assert.assertEquals(true, Brackets.isWellFormed("()()"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("({}{}{}{})"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("[()()]"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("{[][()()]}"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("()()"));
+        Assert.assertEquals(true, BracketsUtils.isWellFormed("{[][][][][]}"));
     }
 
 }
